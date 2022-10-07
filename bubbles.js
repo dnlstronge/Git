0ef734ec8dbd8d9f94bd834b307f2db373951308
Dan Stronge <dnlstronge@gmail.com>
2022-10-07 11:48:50 +0100

    Create: created vars and function to iterate fix: fixed NaN arror test: tested all vars and function in console

---
+++ bubbles.js
@@ -0,0 +1,36 @@
+//create program which returns solutions with highest scores from an array
+
+
+// Globals
+
+var scores = [60, 50, 60, 58, 54, 54, 
+              58, 50, 52, 54, 48, 69,
+              34, 55, 51, 52, 44, 51,
+              69, 64, 66, 55, 52, 61,
+              46, 31, 57, 52, 44, 18,
+              41, 53, 55, 61, 51, 44];
+var numTests = scores.length
+// console.log(numTests); output: 36
+var numHigh = Math.max.apply(Math, scores);
+// console.log(numHigh);  output 69
+var checkArray = find(numHigh,scores);
+// console.log(checkArray); output: 11, 18
+
+// Function (checks array for matches)
+
+function find(numHigh, scores) {
+    var results = [];
+    var idx = scores.indexOf(numHigh);
+    while (idx != -1) {
+        results.push(idx);
+        idx = scores.indexOf(69, idx + 1);
+    }
+    return results;
+}
+
+
+
+
+
+
+
