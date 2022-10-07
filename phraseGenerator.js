c51ddcde7155cc010f154478305b6d8d618e6037
Dan Stronge <dnlstronge@gmail.com>
2022-10-07 08:58:51 +0100

    Create: created JS file - first arrays added

---
+++ phraseGenerator.js
@@ -0,0 +1,6 @@
+// create random phrase generator using arrays
+
+function makePhrases () {
+    var words1 = ["24/7", "multi-tier", "30,000 foot", "B-to-B", "win-win];
+    var words2 = ["empowered", "value-added", "orientated", "focused", "aligned"];
+}
