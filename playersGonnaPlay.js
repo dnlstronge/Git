0748980e014c66fafd3e3d479a1c70e17d16ec40
Dan Stronge <dnlstronge@gmail.com>
2022-10-08 11:59:51 +0100

    fix: fixed syntax error causing undefined object fix: fixed error in 'add player' method. test: tested whether new player would be pushed to object

--- playersGonnaPlay.js
+++ playersGonnaPlay.js
@@ -1,8 +1,9 @@
 
-//arrays and objects and arrays and .......
-//good luck with this one.....
+
 
 const team = {
+    
+  
     _players: [
 
     {firstName: 'Dave',
@@ -17,9 +18,50 @@ const team = {
       lastName: 'Nomates',
       age: 22
       }],
+
     _games: [
-      { opponent: , teamPoints: , opponentPoints: },
-      { opponent: , teamPoints: , opponentPoints: },
-      { opponent: , teamPoints: , opponentPoints: }
-    ]
+
+    { opponent: 'The Burglers' , teamPoints: 13, opponentPoints: 12 },
+
+    { opponent: 'The Rapscallions' , teamPoints: 5 , opponentPoints: 9 },
+
+    { opponent: 'Shifty Balls FC' , teamPoints: 7, opponentPoints: 8 }
+    ],
+
+    get players() {return this._players;
+    },
+    get games() {return this._games;
+    },
+    
+// method
+   addPlayer(newFirstName, newLastName, newAge){
+      
+      let player = {
+      firstName: newFirstName,
+      lastName: newLastName,
+      age: newAge
+      };
+
+      this.players.push(player);
+   
+   }
+  
 };
+
+// Test: add team player using add player method and log
+
+team.addPlayer('Randy', 'Bobandi', 45);
+console.log(team.players);
+    
+/* 
+
+logs to console:
+
+node playersGonnaPlay.js
+[
+  { firstName: 'Dave', lastName: 'Ballsack', age: 32 },
+  { firstName: 'Harry', lastName: 'Flopsweat', age: 28 },
+  { firstName: 'Billy', lastName: 'Nomates', age: 22 },
+  { firstName: 'Randy', lastName: 'Bobandi', age: 45 }
+]
+*/
