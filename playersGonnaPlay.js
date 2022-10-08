1225d26d7e3693ba07090f8fad54c3d7c57e12bd
Dan Stronge <dnlstronge@gmail.com>
2022-10-08 09:49:53 +0100

    create: started task - array/objects/property

---
+++ playersGonnaPlay.js
@@ -0,0 +1,25 @@
+
+//arrays and objects and arrays and .......
+//good luck with this one.....
+
+const team = {
+    _players: [
+
+    {firstName: 'Dave',
+      lastName: 'Ballsack',
+      age: 32},
+
+     {firstName: 'Harry',
+      lastName: 'Flopsweat',
+      age: 28},
+
+     {firstName: 'Billy',
+      lastName: 'Nomates',
+      age: 22
+      }],
+    _games: [
+      { opponent: , teamPoints: , opponentPoints: },
+      { opponent: , teamPoints: , opponentPoints: },
+      { opponent: , teamPoints: , opponentPoints: }
+    ]
+};
