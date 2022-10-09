0ad25656c4786401eabec6e76153bcea35e96124
Dan Stronge <dnlstronge@gmail.com>
2022-10-09 10:24:55 +0100

    create: added obj 'cadi'

--- chevy.js
+++ chevy.js
@@ -1,7 +1,7 @@
 // objects exercise - create chevy
 
 var chevy = {
-    make: "chevy",
+    make: "Chevrolet",
     model: "Bel Air",
     year: 1957,
     color: "red",
@@ -9,4 +9,17 @@ var chevy = {
     convertible: false,
     mileage: 1021,
 };
-console.log(chevy)
+
+// add new obj for cadillac
+
+var cadi = {
+    make: "General Motors",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false,
+    mileage: 12,892,
+
+}
+//console.log(chevy)
