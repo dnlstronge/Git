d172ea1c73922f873a9a55b0929ff8550d432d8f
Dan Stronge <dnlstronge@gmail.com>
2022-10-09 10:20:54 +0100

    create: created obj chore: tidied folder + version control

---
+++ chevy.js
@@ -0,0 +1,12 @@
+// objects exercise - create chevy
+
+var chevy = {
+    make: "chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021,
+};
+console.log(chevy)
