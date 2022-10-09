61c2f2eeb1301e90efeabdb2e765ffd485e6460f
Dan Stronge <dnlstronge@gmail.com>
2022-10-09 12:31:30 +0100

    Fix: fixed syntax error Test: tested various obj in function - clear

--- chevy.js
+++ chevy.js
@@ -35,25 +35,37 @@ var fiat = {
     convertible: false,
     mileage: 88000,
 
-}
-fiat.needsWashing = true;
-
-fiat.mileage = 10000;
-fiat.mileage = 1000;
-
-var miles = fiat.mileage;
-        if (miles < 2000) {
-    console.log("Buy it!");
-}       else {
-    console.log("Don't Buy it!")
-}
-
-if (fiat.year < 1965) {
-    classic = true;
-}
-for (var i = 0; i < fiat.passengers; i++) {
-    console.log(i);
 };
 
-console.log(fiat);
-console.log (classic);
+var taxi = {
+        make: "Webville Motors",
+        model: "Taxi",
+        year: 1955,
+        color: "yeller",
+        passengers: 4,
+        convertible: false,
+        mileage: 281341
+    
+
+};
+
+//new exercise - add fucntion to determine whether user purchases
+
+function prequal(car) {
+    if (car.mileage > 10000) {
+        return false;
+
+    } else if (car.mileage > 1960) {
+        return false;
+    }
+    return true;
+}
+
+var worthALook = prequal(chevy);
+if (worthALook) {
+    console.log("Yo, yo, yo check out this " + chevy.make + " " + chevy.model)
+} else {
+    console.log("You should really pass on the " + chevy.make + " " + chevy.model)
+}
+
+
