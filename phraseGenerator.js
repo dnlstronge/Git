af303b753d17119e87d749a5a6a194a8ca616d3f
dnlstronge <114242578+dnlstronge@users.noreply.github.com>
2022-10-13 15:03:10 +0100

    Update phraseGenerator.js
    fix syntax error

--- phraseGenerator.js
+++ phraseGenerator.js
@@ -6,9 +6,9 @@ function makePhrases () {
     var words2 = ["empowered", "value-added", "orientated", "focused", "aligned"];
     var words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
 
-    var rand1 = Math.floor(Math.random * words1.length);
-    var rand2 = Math.floor(Math.random * words2.length);
-    var rand3 = Math.floor(Math.random * words3.length);
+    var rand1 = Math.floor(Math.random() * words1.length);
+    var rand2 = Math.floor(Math.random() * words2.length);
+    var rand3 = Math.floor(Math.random() * words3.length);
 
     var phrase = words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
 
