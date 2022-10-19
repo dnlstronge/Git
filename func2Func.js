34c12cef573acec04f3c270c7639267d9fe3a53a
Dan Stronge <dnlstronge@gmail.com>
2022-10-19 12:00:24 +0100

    // functions and functions exercise created array

---
+++ func2Func.js
@@ -0,0 +1,8 @@
+var passengers = [
+    {name: "Dr Proctor", paid: true},
+    {name: "Ms Round", paid: true},
+    {name: "Mr Jefferson", paid: false},
+    {name: "Lord Maltese", paid: true}
+
+
+]
