34cf3a1f3e30f501cbdd2b4c14b4b32eae040adb
Dan Stronge <dnlstronge@gmail.com>
2022-10-20 10:09:22 +0100

    create: add array with various product objects.

---
+++ colastore.js
@@ -0,0 +1,9 @@
+var products = [ {name: "Grapefruit", calories: 170, color: "red", sold: 8200},
+                 {name: "Orange", calories: 160, color: "orange", sold: 12101},
+                 {name: "Cola", calories: 210, color: "caramel", sold: 25412},
+                 {name: "Diet Cola", calories: 0, color: "caramel", sold: 43922},
+                 {name: "lemon", calories: 200, color: "clear", sold: 14983},
+                 {name: "Raspberry", calories: 180, color: "pink", sold: 9427},
+                 {name: "Root Beer", calories: 200, color: "caramel", sold: 9909},
+                 {name: "Water", calories: 0, color: "clear", sold: 62123}
+];
