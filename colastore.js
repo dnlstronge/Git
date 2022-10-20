b6f7df7d449e309ba2baa5bbd818fa79ed87045e
Dan Stronge <dnlstronge@gmail.com>
2022-10-20 10:14:42 +0100

    create: add function to compare by number of obj sold

--- colastore.js
+++ colastore.js
@@ -1,3 +1,6 @@
+
+// the company requires a means of sorting their stock by different values of their product
+
 var products = [ {name: "Grapefruit", calories: 170, color: "red", sold: 8200},
                  {name: "Orange", calories: 160, color: "orange", sold: 12101},
                  {name: "Cola", calories: 210, color: "caramel", sold: 25412},
@@ -7,3 +10,17 @@ var products = [ {name: "Grapefruit", calories: 170, color: "red", sold: 8200},
                  {name: "Root Beer", calories: 200, color: "caramel", sold: 9909},
                  {name: "Water", calories: 0, color: "clear", sold: 62123}
 ];
+
+
+// start with items sold in ascending order
+// compare function will be passed to objects not numbers
+
+function compareSold (colaA, colaB) {
+    if (colaA.sold > colaB.sold) {
+        return 1;
+    } else if (colaA.sold == colaB.sold) {
+        return 0;
+    } else {
+        return -1;
+    }
+    }
