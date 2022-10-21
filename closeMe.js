74c396cdf423b823e4349cb4cfd2b6c10b2338b9
Dan Stronge <dnlstronge@gmail.com>
2022-10-21 09:19:32 +0100

    fix: change if statement to make clearer

--- closeMe.js
+++ closeMe.js
@@ -1,26 +1,29 @@
 
 //write a simple code which helps explain closures
 
-var closures = 9
-var closures2 = 10
+var closures = 1
+var closures2 = 2
 
 var closures3 = function testme() {
 
     // variabless x, y are declared inside testme
-    let x = (closures + 1)
-    let y = (closures2 + 1)
+    let x = closures + 1;
+    let y = closures2 + 1;
     // new function expression beep is nested inside testme
     function beep () {
-        if(x + y < 17) {return false;}
-     else (x + y == 17); {
+        if(x < y) {return false;}
+     else if (x > y); {
     /*x + y are not declared in this function body
-    but because they are not global and declared inside the function beep is nested in
+    but because they are not global and declared inside the function 'beep' is nested in
     they are available the environment */
      return true;
     }
+    
 }
 return beep();
 }
 console.log(closures3());
 
+
 // test: change values for closures 1/2, expected output T/F (clear)
+// slight issue with if statement (changed)
