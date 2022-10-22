5af57af0a5ecb0f44a3457aac9f535fc2682f151
Dan Stronge <dnlstronge@gmail.com>
2022-10-22 12:11:53 +0100

    create: add functions for 3 events

---
+++ eventsExercise.js
@@ -0,0 +1,32 @@
+// event handlers and events excercise: 
+
+// These variables store the boxes on the side
+let itemOne = document.getElementById('list-item-one');
+let itemTwo = document.getElementById('list-item-two');
+let itemThree = document.getElementById('list-item-three');
+let itemFour = document.getElementById('list-item-four');
+let itemFive = document.getElementById('list-item-five');
+let resetButton = document.getElementById('reset-button');
+
+// This function programs the "Reset" button to return the boxes to their default styles
+let reset = function() {
+  itemOne.style.width = ''
+  itemTwo .style.backgroundColor = ''
+  itemThree.innerHTML = 'The mouse must leave the box to change the text'
+  itemFive.style.display = "none"
+};
+resetButton.onclick = reset;
+
+changetext() => {
+  itemThree.innerHTML = ('The mouse has left the element')
+}
+
+function changeBackground() {
+  itemTwo.style.backgroundColor = '';
+}
+
+function increaseWidth() {
+  itemOne.style.width = '500px';
+}
+itemOne.addEventListener('mouseover', increaseWidth);
+itemTwo.addEventListener('mouseup', changeBackground);
