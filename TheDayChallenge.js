531656f7a1ec32d13ea198cce26cf521ca686def
Dan Stronge <dnlstronge@gmail.com>
2022-10-23 17:59:16 +0100

    create:start challenge

---
+++ TheDayChallenge.js
@@ -0,0 +1,15 @@
+/*Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday" */
+
+function whatday(num) {
+
+    
+}
+
+    
