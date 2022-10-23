3803e80f69003971c2ab61805c5cee777052d5f3
Dan Stronge <dnlstronge@gmail.com>
2022-10-23 18:28:17 +0100

    create: create switch statement as solution

--- TheDayChallenge.js
+++ TheDayChallenge.js
@@ -9,7 +9,29 @@
 
 function whatday(num) {
 
+    switch (num) {
+        case 1:
+        return "Sunday";
+        case 2:
+          return "Monday";
+        case 3:
+          return "Tuesday";
+        case 4:
+          day = "Wednesday";
+        case 5:
+          day = "Thursday";
+        case 6:
+          return "Friday"
+        case 7: 
+          return "Saturday" 
+        case num > 7 || num < 1 || num == NaN:
+        return  "Wrong, please enter a number between 1 and 7"
+       
+      
     
 }
+}
 
-    
+//tried a switch statement as solution
+//testcode:
+console.log(whatday(4));
