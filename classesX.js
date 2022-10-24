663649fd850b79962a8534b8e578a030873df165
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-10-24 15:29:48 +0100

    Update classesX.js
    add child + new nurse

--- classesX.js
+++ classesX.js
@@ -6,13 +6,25 @@ class HospitalEmployee {
     this._name = name;
     this._remainingVacationDays = 20;
   }
+  
   get name() {
     return this._name;
   }
+  
   get remainingVacationDays() {
     return this._remainingVacationDays;
   }
+  
   takeVacationDays(daysOff) {
-    this._remainingVacationDays = this._remainingVacationDays - daysOff;
-  } 
+    this._remainingVacationDays -= daysOff;
+  }
 }
+
+class Nurse extends HospitalEmployee {
+ constructor(name, certifications) {
+   super(name);
+   this._certifications = certifications;
+ } 
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
