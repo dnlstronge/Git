ab9f6893ebeb697eb59bb58cd1fb46aedc74c4c6
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-10-24 14:54:55 +0100

    Update classesX.js
    Create new constructor

--- classesX.js
+++ classesX.js
@@ -1,3 +1,9 @@
 //exercise in creating classess and using inheritance
 // create parent
 
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+}
