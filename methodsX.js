2f16af7ef23f1f3e92370c78a8b0871f2771bf13
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-10-24 14:16:25 +0100

    Update methodsX.js
    create: add method call

--- methodsX.js
+++ methodsX.js
@@ -1,5 +1,4 @@
 // classes and methods exercise
-
 class Surgeon {
   constructor(name, department) {
     this._name = name;
@@ -26,3 +25,11 @@ class Surgeon {
 
 const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
 const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+
+// log name of surgeon romero:
+
+console.log(surgeonRomero.name);
+
+//call vacationdays method with an input of 3 on surgeon romero:
+
+surgeonRomero.takeVacationDays(3);
