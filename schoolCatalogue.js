b342bbf50d06ad114e520eccad986e56766ffa18
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-10-25 11:43:47 +0100

    add function + add static function

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -16,9 +16,18 @@ class school{
     return this._numberOfStudents;
   }
   set numberOfStudents(value) {
-    if (value = NaN) {console.log("Invalid input: numberOfStudents must be set to a Number.")
+    if (value.isNaN()) {console.log("Invalid input: numberOfStudents must be set to a Number.")
     } else {
       this.numberOfStudents = value;
     }
 }
+quickFacts() {
+  console.log(this.name + "educates " + this.numberOfStudents + "students at the " + this.level + "school level.")
+ }
+static pickSubstituteTeacher (substituteTeachers) {
+  const randoI = Math.floor(substituteTeachers.length * Math.Random());
+return substituteTeachers[randoI];
+}
+
+
 }
