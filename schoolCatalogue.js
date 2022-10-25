62f10a3ec17417b6966ef550e0af36008d97d74d
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-10-25 10:46:39 +0100

    Add class obj and getters

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -1,1 +1,18 @@
 //excercise to make a fictional school catalogue
+
+class school{
+  constructor(name, level, numberOfStudents) {
+    _name = name;
+    _level = level;
+    _numberOfStudents = numberOfStudents;
+  }
+  get name() {
+    return this._name;
+  }
+  get level() {
+    return this._level;
+  }
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+}
